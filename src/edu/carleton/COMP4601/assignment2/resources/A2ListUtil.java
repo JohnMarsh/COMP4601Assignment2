da867d5f78bc097c34067fc49c932e5186b22e0f
John Marsh <john.michael.marsh@gmail.com>
2015-02-16 16:33:36 -0500

    list util werking

--- src/edu/carleton/COMP4601/assignment2/resources/A2ListUtil.java
+++ src/edu/carleton/COMP4601/assignment2/resources/A2ListUtil.java
@@ -5,12 +5,44 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.mongodb.DBObject;
+
+import edu.carleton.COMP4601.utility.SearchServiceManager;
+
 @Path("/sda/list")
 public class A2ListUtil extends AbstractA2Resource {
-	
+
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String list() {
-		return "";
+		try {
+			JSONArray array = new JSONArray(SearchServiceManager.list());
+			if (array.length() == 0) {
+				return "<p>No Services Found</p>";
+			}
+			StringBuilder sb = new StringBuilder();
+			sb.append("<h1>Services</h1>\n");
+			sb.append("<ul>");
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject o = array.getJSONObject(i);
+				sb.append("<li>");
+				sb.append("<a href=\"" + o.get("url") + "\">");
+				sb.append(o.get("name"));
+				sb.append("</a>");
+				sb.append("</li>");
+			}
+			sb.append("</ul>");
+
+			return sb.toString();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "<p>Error Occured</p>";
+		}
+
 	}
 }
